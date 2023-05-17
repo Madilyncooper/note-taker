1026ca3015166a9ed558ea4122f77a599581b495
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-17 14:41:46 -0700

    Added process.env.PORT to solve heroku deploying issue

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const notesDb = require('./db/db.json');
 const uuid = require('uuid');
 const { readFromFile, readAndAppend } = require('./helpers/fsutils');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -12,8 +11,6 @@ app.use(clog);
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
-
 
 
 app.get('/notes', (req, res) =>{ 
