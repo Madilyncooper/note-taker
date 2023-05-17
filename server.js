f63387dd89f0d7d958fdc3242d786abbfda0ca18
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-17 14:38:49 -0700

    Moved get request with * to bottom of server.js page so that it will not interfere with the other routes

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
+
+
 
 app.get('/notes', (req, res) =>{ 
   res.sendFile(path.join(__dirname, './public/notes.html'))
@@ -47,13 +46,9 @@ if(title && text) {
 }
 });
 
-
-
-
-
-
-
-
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
